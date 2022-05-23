daf73b6181163100087994ecd7de012514f4d03b
Mujtaba <mojtaba.khawari@gmail.com>
2022-05-23 17:43:36 +0300

    main.js edited and second part added

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,20 @@ const navItems=document.querySelector('.nav-item');
 let showMenu=false;
 menuBtn.addEventListener('click',toggleMenu);
 
+function toggleMenu(){
+    if(!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show') );            
+        showMenu=true;
+    }else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show') );            
+        showMenu=false;
+    }
+}
